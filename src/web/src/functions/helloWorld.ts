8beaa5a0ebee9c601dee6d3073c50137d16a2d43
kkgthb <44843658+kkgthb@users.noreply.github.com>
2026-09-22 16:59:28 -0500

    Hopefully got an Azure Functions build working

---
+++ src/web/src/functions/helloWorld.ts
@@ -0,0 +1,13 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function helloWorld(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+    const name = request.query.get('name') || await request.text() || 'World';
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('helloWorld', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: helloWorld
+});
